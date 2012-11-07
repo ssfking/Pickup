a326a3e213981263ea212f84e23cbd55b84399b8
Spencer Shing Foon King <ssking@corn13.stanford.edu>
2012-11-07 14:59:43 -0800

    included gameDetail.php

---
+++ gameDetail.php
@@ -0,0 +1,70 @@
+CTYPE html>
+<html>
+
+<head>
+        <title>Pickup - Find a Game</title> 
+        <meta charset="utf-8">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
+
+        <link rel="stylesheet" href="jquery.mobile-1.2.0.css" />
+        <link rel="stylesheet" href="style.css" />
+        <link rel="apple-touch-icon" href="appicon.png" />
+        <link rel="apple-touch-startup-image" href="startup.png">
+
+        <script src="jquery-1.8.2.min.js"></script>
+        <script src="jquery.mobile-1.2.0.js"></script>
+
+        <link rel="stylesheet" href="jquery.mobile-1.2.0.css" />
+
+</head>
+
+<body>
+
+<div data-role="page" id="gameDetail">
+	<div data-role="header" data-id="findHeader" data-position="fixed">
+                <a href="findGame.php" data-rel="back">Back</a>
+                <h1>Game</h1>
+
+        </div><!-- /header -->
+
+        <div data-role="content">
+
+	
+	<p>
+	Game Type: Soccer
+	</p>
+	
+	<p>
+	Date: 10th Octeber, 2012
+	</p>
+
+	<p>
+	Time: 11:00am - 1pm
+	</p>
+	
+	<p>
+	Location: Toyon fields
+	</p>
+	
+	<p>
+	Number of people signed up: 6
+	</p>
+
+	<p>
+	Min/Max Capacity: 5/22
+	</p> 
+	
+	<p>
+	<form action="#" method="post">
+	<input type="hidden" name="status" value="Join" />
+	<input type="submit" value="Join"/>
+	</form>
+	</p>
+	</div>
+</div>
+</body>
+</html>
+
+
